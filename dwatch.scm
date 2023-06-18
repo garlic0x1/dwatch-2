d638c171c77c304c355187b1d20a9416261ff1b2
garlic0x1 <43155857+garlic0x1@users.noreply.github.com>
2023-06-17 23:21:03 -0500

    Add files via upload

---
+++ dwatch.scm
@@ -0,0 +1,46 @@
+#!guile \
+-e main -s
+!#
+
+(use-modules
+ (srfi srfi-19)
+ (srfi srfi-13)
+ (ice-9 ftw)
+ (ice-9 popen))
+
+(define (worker cmd)
+  (let ((port (open-output-pipe "")))
+    (lambda ()
+      (close-pipe port)
+      (set! port (open-output-pipe cmd)))))
+
+(define (dir-item? it)
+  (and (list? it)
+       (vector? (cadr it))))
+
+(define (file-modified dir-item)
+  (let* ((name (car dir-item))
+	 (data (cadr dir-item))
+	 (modified (stat:mtime data)))
+    (cons name modified)))
+
+(define (right-type? types dir-item)
+  (if (null? types) #f
+      (or (string-suffix? (car types) (car dir-item))
+	  (right-type? (cdr types) dir-item))))
+
+(define (watch-dir restart file-types modified-alist)
+  (let* ((items (file-system-tree "."))
+	 (all-files (filter dir-item? items))
+	 (files (filter (lambda (x) (right-type? file-types x)) all-files))
+	 (new-alist (map file-modified files)))
+    (when (not (equal? modified-alist new-alist))
+      (restart))
+    (sleep 1)
+    (watch-dir restart file-types new-alist)))
+
+(define (main . args)
+  (let ((config (call-with-input-file ".dwatch" read)))
+    (watch-dir (worker (cdr (assoc 'cmd config)))
+	       (cdr (assoc 'file-types config))
+	       '())))
